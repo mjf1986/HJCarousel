4fa5654b131e50bd4810c363e4e1d1820352b868
jianfengmo <420664652@qq.com>
2016-07-05 13:47:14 +0800

    Initial Commit

---
+++ HJCarousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJCarousel
+//
+//  Created by 剑锋 莫 on 16/7/5.
+//  Copyright © 2016年 剑锋 莫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HJCarousel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HJCarousel
+//
+//  Created by 剑锋 莫 on 16/7/5.
+//  Copyright © 2016年 剑锋 莫. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
